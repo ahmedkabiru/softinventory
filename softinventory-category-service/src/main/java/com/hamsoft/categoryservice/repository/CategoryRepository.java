b35c0357ba10ee1507e8ce27233932f4dc8ddd6c
Ahmed Kabiru <opeyemi.kabiru@yahoo.com>
2021-03-18 22:40:26 +0100

    add category repository

---
+++ softinventory-category-service/src/main/java/com/hamsoft/categoryservice/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.hamsoft.categoryservice.repository;
+
+import com.hamsoft.categoryservice.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
